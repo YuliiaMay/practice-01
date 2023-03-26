8baac81fe6695ba5636de38fa4b9ccb462de7468
Vitaly Nelyub <nelyub86@ukr.net>
2023-03-26 16:06:28 +0300

    Vitaly

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,10 +4,32 @@ import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const iconArr = [
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <FaRegThumbsUp />,
+  <GiTreeDoor />,
+];
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map(({ id, title, total }, index) => {
+          // console.log(iconArr)
+          // console.log(index)
+          // console.log(iconArr[index])
+          return (
+            <StatisticItem
+              key={id}
+              title={title}
+              total={total}
+              icon={iconArr[index]}
+            />
+          );
+        })}
+      </StatisticsList>
     </>
   );
 };
