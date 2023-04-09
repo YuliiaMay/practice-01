adbff2726bce929f91ceafb06d951c44093eff27
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-04-09 16:12:19 +0300

    Update Todos.jsx

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,31 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  componentDidUpdate() {
+    console.log(this.state.todos);
+  }
+
+  onSubmit = text => {
+    this.addTodo(text);
+  };
+
+  addTodo = text => {
+    const todo = {
+      text,
+      id: nanoid(),
+    };
+    this.setState(prevState => ({ todos: [...prevState.todos, todo] }));
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <>
+        <SearchForm onSubmit={this.onSubmit} />
+      </>
+    );
   }
 }
